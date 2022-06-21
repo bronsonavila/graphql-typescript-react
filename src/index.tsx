4dd5db055fe7af29e3b8ee46c9a6a151bd889cf1
Bronson Avila <bronson.avila@bbot.menu>
2022-06-21 10:49:37 -1000

    Initialize Apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: 'https://spacexdata.herokuapp.com/graphql',
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
